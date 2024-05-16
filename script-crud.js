cd4eed7e7b358e1d0da4e7b010cfeedb422a35ab
ledsouza <leandro.souza.159@gmail.com>
2024-05-16 18:14:39 -0300

    código refatorado

--- script-crud.js
+++ script-crud.js
@@ -1,8 +1,7 @@
 const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
-const textareaAdicionarTarefa = document.querySelector(".app__form-textarea");
 
-const tarefas = [];
+let tarefas = [];
 
 btnAdicionarTarefa.addEventListener("click", () => {
   formAdicionarTarefa.classList.toggle("hidden");
@@ -10,8 +9,9 @@ btnAdicionarTarefa.addEventListener("click", () => {
 
 formAdicionarTarefa.addEventListener("submit", (evento) => {
   evento.preventDefault();
+  const textarea = document.querySelector(".app__form-textarea");
   const tarefa = {
-    descricao: textareaAdicionarTarefa.value,
+    descricao: textarea.value,
   };
   tarefas.push(tarefa);
   localStorage.setItem("tarefas", JSON.stringify(tarefas));
