7eb7ae4eb207d10c9ada79444b32e6efb31c142b
ledsouza <leandro.souza.159@gmail.com>
2024-05-16 09:49:20 -0300

    atualizado alterarContexto

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const focoBt = document.querySelector(".app__card-button--foco")
 const curtoBt = document.querySelector(".app__card-button--curto")
 const longoBt = document.querySelector(".app__card-button--longo")
 const imagemContexto = document.querySelector(".app__image")
+const titulo = document.querySelector(".app__title")
 
 // Contextos
 focoContexto = "foco"
@@ -24,4 +25,27 @@ longoBt.addEventListener("click", () => {
 function alterarContexto(contexto) {
     html.setAttribute("data-contexto", contexto)
     imagemContexto.setAttribute("src", `imagens/${contexto}.png`)
+    
+    switch (contexto) {
+        case focoContexto:
+            titulo.innerHTML = `
+            Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case curtoContexto:
+            titulo.innerHTML = `
+            Que tal dar uma respirada?<br> 
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+            break;
+        case longoContexto:
+            titulo.innerHTML = `
+            Hora de voltar à superfície.<br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `
+            break;
+        default:
+            break;
+    }
 }
