f5e749b174ffcf0d0d490b954b3ee77b5188adc5
ledsouza <leandro.souza.159@gmail.com>
2024-05-16 11:36:32 -0300

    adicionado event listener no toggle de música

--- script.js
+++ script.js
@@ -1,60 +1,72 @@
-const html = document.querySelector("html")
-const focoBt = document.querySelector(".app__card-button--foco")
-const curtoBt = document.querySelector(".app__card-button--curto")
-const longoBt = document.querySelector(".app__card-button--longo")
-const imagemContexto = document.querySelector(".app__image")
-const titulo = document.querySelector(".app__title")
-const botoes = document.querySelectorAll(".app__card-button")
+const html = document.querySelector("html");
+const focoBt = document.querySelector(".app__card-button--foco");
+const curtoBt = document.querySelector(".app__card-button--curto");
+const longoBt = document.querySelector(".app__card-button--longo");
+const imagemContexto = document.querySelector(".app__image");
+const titulo = document.querySelector(".app__title");
+const botoes = document.querySelectorAll(".app__card-button");
+const musicaToggle = document.querySelector("#alternar-musica");
+const musica = new Audio("sons/luna-rise-part-one.mp3");
+musica.loop = true;
 
 // Contextos
-focoContexto = "foco"
-curtoContexto = "descanso-curto"
-longoContexto = "descanso-longo"
+focoContexto = "foco";
+curtoContexto = "descanso-curto";
+longoContexto = "descanso-longo";
 
-botaoAtivo = "active"
+// Estilização de ativo
+botaoAtivo = "active";
+
+musicaToggle.addEventListener("change", () => {
+  if (musica.paused) {
+    musica.play();
+  } else {
+    musica.pause();
+  }
+});
 
 focoBt.addEventListener("click", () => {
-    alterarContexto(focoContexto)
-    focoBt.classList.add(botaoAtivo)
-})
+  alterarContexto(focoContexto);
+  focoBt.classList.add(botaoAtivo);
+});
 
 curtoBt.addEventListener("click", () => {
-    alterarContexto(curtoContexto)
-    curtoBt.classList.add(botaoAtivo)
-})
+  alterarContexto(curtoContexto);
+  curtoBt.classList.add(botaoAtivo);
+});
 
 longoBt.addEventListener("click", () => {
-    alterarContexto(longoContexto)
-    longoBt.classList.add(botaoAtivo)
-})
+  alterarContexto(longoContexto);
+  longoBt.classList.add(botaoAtivo);
+});
 
 function alterarContexto(contexto) {
-    html.setAttribute("data-contexto", contexto)
-    imagemContexto.setAttribute("src", `imagens/${contexto}.png`)
-    botoes.forEach(function (botao) {
-        botao.classList.remove(botaoAtivo)
-    })
-    
-    switch (contexto) {
-        case focoContexto:
-            titulo.innerHTML = `
+  html.setAttribute("data-contexto", contexto);
+  imagemContexto.setAttribute("src", `imagens/${contexto}.png`);
+  botoes.forEach(function (botao) {
+    botao.classList.remove(botaoAtivo);
+  });
+
+  switch (contexto) {
+    case focoContexto:
+      titulo.innerHTML = `
             Otimize sua produtividade,<br>
             <strong class="app__title-strong">mergulhe no que importa.</strong>
-            `
-            break;
-        case curtoContexto:
-            titulo.innerHTML = `
+            `;
+      break;
+    case curtoContexto:
+      titulo.innerHTML = `
             Que tal dar uma respirada?<br> 
             <strong class="app__title-strong">Faça uma pausa curta!</strong>
-            `
-            break;
-        case longoContexto:
-            titulo.innerHTML = `
+            `;
+      break;
+    case longoContexto:
+      titulo.innerHTML = `
             Hora de voltar à superfície.<br>
             <strong class="app__title-strong">Faça uma pausa longa.</strong>
-            `
-            break;
-        default:
-            break;
-    }
+            `;
+      break;
+    default:
+      break;
+  }
 }
